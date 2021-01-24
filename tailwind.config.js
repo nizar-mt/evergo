b40cbaccfd49ac604c40e52dca34ea5828aa5698
nizar-mt <nizar@mannai.tech>
2021-01-24 11:27:10 +0300

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#1BAAA5',
+                'green-primary': '#8DAA1B',
+                'green-secondary': ' #2BE9C7',
+                'blue-primary': '#428EE7',
+                'pink-primary': '#B65BEE',
+                'gray-primary': '#323A3A'
+            },
+            height: {
+                '240': '240px',
+                '450': '450px',
+                '650': '650px',
+                '1000': '1000px',
+                '1100': '1100px',
+                '1200': '1200px'
+            },
+
+
+
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
